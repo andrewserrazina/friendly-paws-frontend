70ae423d9ba0d114fb094963ad6fb022e6ae9950
andrewserrazina <aserrazina101@gmail.com>
2025-02-20 20:21:57 -0500

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 const Dashboard = () => {
